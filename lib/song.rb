2aea9defdec963491c53c7e16864dd88952036ea
Precious Humphrey <phumphrey23@gmail.com>
2018-05-20 19:17:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,33 @@
 class Song
  @@count = 0
+ @@genres = []
+ @@artists = []
+ @@genre_count = {}
+ @@artist_count = {}
 
  def initialize
   @@count+=1
+  @@genres+=1
+  @@artists+=1
+  
  end
 
  def self.count
     @@count
   end
 
-  def artists
+  def self.genres
+    @@genres.uniq
+  end
 
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    @@genres.each do
+      
+    end
   end
 
 end
